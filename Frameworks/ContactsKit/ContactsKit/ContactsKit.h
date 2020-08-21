30895387743b3635ebe6688cd6e5777bac628b26
Daniel Sumara <daniel.sumara@gmail.com>
2020-08-21 13:08:35 +0200

    Add frameworks

---
+++ Frameworks/ContactsKit/ContactsKit/ContactsKit.h
@@ -0,0 +1,19 @@
+//
+//  ContactsKit.h
+//  ContactsKit
+//
+//  Created by Daniel Sumara on 21/08/2020.
+//  Copyright © 2020 Daniel Sumara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContactsKit.
+FOUNDATION_EXPORT double ContactsKitVersionNumber;
+
+//! Project version string for ContactsKit.
+FOUNDATION_EXPORT const unsigned char ContactsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactsKit/PublicHeader.h>
+
+
---
+++ Frameworks/ContactsKit/ContactsModels/ContactsModels.h
@@ -0,0 +1,19 @@
+//
+//  ContactsModels.h
+//  ContactsModels
+//
+//  Created by Daniel Sumara on 21/08/2020.
+//  Copyright © 2020 Daniel Sumara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContactsModels.
+FOUNDATION_EXPORT double ContactsModelsVersionNumber;
+
+//! Project version string for ContactsModels.
+FOUNDATION_EXPORT const unsigned char ContactsModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactsModels/PublicHeader.h>
+
+
