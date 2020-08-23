64e760a8298692ea70a596f7698c318b696b46e8
Daniel Sumara <daniel.sumara@gmail.com>
2020-08-23 12:08:02 +0200

    Target for contact details Typos fix

---
+++ Frameworks/Business/ContactDetails/ContactDetails.h
@@ -0,0 +1,19 @@
+//
+//  ContactDetails.h
+//  ContactDetails
+//
+//  Created by Daniel Sumara on 23/08/2020.
+//  Copyright © 2020 Daniel Sumara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ContactDetails.
+FOUNDATION_EXPORT double ContactDetailsVersionNumber;
+
+//! Project version string for ContactDetails.
+FOUNDATION_EXPORT const unsigned char ContactDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ContactDetails/PublicHeader.h>
+
+
